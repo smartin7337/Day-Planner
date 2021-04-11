82cd6538df4112ee327e90c69a10f8b97b1e0604
Vossie <sv7337@outlook.com>
2021-04-11 14:55:51 -0400

    changed the font and styling a bit, fixed my variables

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
 $(document).ready(function(){
-    /* Display the current day of the week and date
-    at the top of the page */
+    /* display current time/day at the top of page */
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
     
-    var timeArray = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"]
-    var currentTime = moment().format("h A");
+    var hourlyBlock = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"]
+    var localTime = moment().format("h A");
     
     
-    // Set each span text to the correct hour between 9am and 5pm
+    /* set each block to the correct hour between 9-5 */
     $(".Hour").each(function(i){
         $(this).text(moment().hour(i+9).format("h A"))
         
     })
-    console.log(currentTime);
+    console.log(localTime);
     
-    
+    /* array for the contents of each block */
     $('.HourRow').each(function(i) {
-        console.log(timeArray.indexOf(currentTime));
-        if (timeArray.indexOf(currentTime) > i) {
+        console.log(hourlyBlock.indexOf(localTime));
+        if (hourlyBlock.indexOf(localTime) > i) {
           $(`#${i}`).addClass('past');
           $(`#saveToDo-${i}`).prop('disabled', true);
           $(`#saveToDo-${i}`).addClass('past');
           $(`#hour-${i}`).addClass('past');
-        } else if (timeArray.indexOf(currentTime) == i) {
+        } else if (hourlyBlock.indexOf(localTime) == i) {
           $(`#${i}`).addClass('present');
           $(`#saveToDo-${i}`).addClass('present');
           $(`#saveToDo-${i}`).prop('disabled', false);
           $(`#hour-${i}`).addClass('present');
-        } else if (timeArray.indexOf(currentTime) < i) {
+        } else if (hourlyBlock.indexOf(localTime) < i) {
           $(`#${i}`).addClass('future');
           $(`#saveToDo-${i}`).addClass('future');
           $(`#saveToDo-${i}`).prop('disabled', false);
           $(`#hour-${i}`).addClass('future');
         }
       });
+
+    });
+  
+    $("button").on("click", function(){
+        $('input, select, textarea').each(function() {
+         var value = $(this).val(),
+             name = $(this).attr('name');
+         localStorage[name] = value;
+                 
+    })});
